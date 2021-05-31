b7a3367e0f923f8fd5d9b9dc0e030563fbb6de2d
Marta <martafontb@gmail.com>
2021-05-31 20:29:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+ScrollTrigger.create({
+		trigger:".box",
+		start:"top top",
+		end:"+=1600",
+		markers:true,
+		pin:true
+})
+
+ScrollTrigger.create({
+	trigger:".img",
+	start:"top top",
+	end:"+=1600",
+	markers:true,
+	pin:true
+})
